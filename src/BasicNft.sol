6b4743d512739be2a6dd1106a1ec67bd57764266
Sergiy Sanin <sanin_sergiy@yahoo.com>
2026-01-13 07:29:36 +0000

    basic test

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.24;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    uint256 private stokenCounter;
+    uint256 private sTokenCounter;
+    mapping(uint256 => string) sTokenIdToUri;
+
     constructor() ERC721("Meowthic", "MEOWTHIC") {
         sTokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        sTokenIdToUri[sTokenCounter] = tokenUri;
+        _safeMint(msg.sender, sTokenCounter);
+        sTokenCounter++;
+    }
 
-    function tikenURI(
+    function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        //
+        return sTokenIdToUri[tokenId];
     }    
 }
